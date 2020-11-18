dcb069bfdc8bfeec366df9f7762ef4e73fa96da6
Sergey Kh <falko.serega2016@yandex.ru>
2020-11-18 18:18:00 +0300

    the GET method for more styles is finished

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-const dist = "./dist/";
+// const dist = "./dist/";
+const dist = '/OpenServer/domains/pictureTest'; 
+
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
